89c922bf6269e4d3c426175f67eeab4dd8dc7b76
Hands-on-robotics <handsonevanoff@gmail.com>
2023-09-29 17:31:26 -0700

    shortUrls now redirect towards the longUrls

--- express_server.js
+++ express_server.js
@@ -5,14 +5,24 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-function generateRandomString() {};
+const generateRandomString = function() {
+  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let randomString = '';
+
+  for (let i = 0; i < 7; i++) {
+    const randomIndex = chars.charAt(Math.floor(Math.random() * chars.length));
+    randomString += randomIndex;
+  }
+
+  return randomString;
+};
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "fsm5xK": "http://www.google.com"
 };
 
 app.get('/', (req, res) => {
@@ -32,8 +42,17 @@ app.get('/hello', (req, res) => {
 });
 
 app.get('/urls', (req, res) => {
-  const templateVars = { urls: urlDatabase }; // So did we create an object around urlDatabase with the key urls?
+  const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  const shortUrl = req.params.id;
+  console.log("This is the database", urlDatabase);
+  console.log(shortUrl);
+  const templateVars = { id: shortUrl, longURL: urlDatabase[shortUrl] };
+  console.log(templateVars);
+  res.render("urls_show", templateVars);
 });
 
 app.get('/urls/new', (req, res) => {
@@ -41,12 +60,13 @@ app.get('/urls/new', (req, res) => {
 });
 
 app.post('/urls', (req, res) => {
-  console.log(req.body);
-  res.send('Ok');
+  const shortUrl = generateRandomString();
+  urlDatabase[shortUrl] = req.body.longURL;
+  console.log(urlDatabase);
+  res.redirect(`/urls/${shortUrl}`);
 });
 
-app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] }; // <--- How do I access the longURL? Is this the correct understanding? How was id: assigned to the short url? Was that done here?
-  res.render("urls_show", templateVars);
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
 });
-
